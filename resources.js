0df40e2f9a1d5fd691c2a2852f442b4ea327f70e
Anhad Srivastava <srivastavaanhad@gmail.com>
2025-03-26 22:58:57 +0530

    grgr

--- resources.js
+++ resources.js
@@ -3,13 +3,63 @@ function filterContent() {
     let input = document.getElementById('searchInput').value.toLowerCase();
     
     // Get all elements that should be filtered
-    let filterItems = document.getElementsByClassName('.jobs');
+    let filterItems = document.getElementsByClassName('jobs');
     
     // Loop through each item and hide or show based on search input
     for (let i = 0; i < filterItems.length; i++) {
         let item = filterItems[i];
         // Check if the item text includes the search input
         if (item.textContent.toLowerCase().includes(input)) {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    }
+}
+
+function filterContent1()
+{
+    let filterItems = document.getElementsByClassName('jobs');
+    for (let i = 0; i < filterItems.length; i++) {
+        let item = filterItems[i];
+        if (item.classList.contains("high-paying")) {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    }
+}
+
+function filterContent2()
+{
+    let filterItems = document.getElementsByClassName('jobs');
+    for (let i = 0; i < filterItems.length; i++) {
+        let item = filterItems[i];
+        if (item.classList.contains("attainable-careers")) {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    }
+}
+function filterContent3()
+{
+    let filterItems = document.getElementsByClassName('jobs');
+    for (let i = 0; i < filterItems.length; i++) {
+        let item = filterItems[i];
+        if (item.classList.contains("part-time-careers")) {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    }
+}
+function filterContent4()
+{
+    let filterItems = document.getElementsByClassName('jobs');
+    for (let i = 0; i < filterItems.length; i++) {
+        let item = filterItems[i];
+        if (item.classList.contains("no-degree-careers")) {
             item.style.display = "block";
         } else {
             item.style.display = "none";
